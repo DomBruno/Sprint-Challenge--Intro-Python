b265c38e3ebbee1d43fa092afa84cb1949de81ee
Dominick Bruno <dbruno@unseen.is>
2020-05-15 21:07:36 -0400

    complete oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,38 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base Class
+class Vehicle:
+    def init(self):
+        pass
+
+# Subclass Flight Vehicle of Vehicle
+class FlightVehicle(Vehicle):
+    def init(self):
+        pass 
+
+# Subclass Starship of Flight Vehicle, Grandchild of Vehicle
+class Starship(FlightVehicle):
+    def init(self):
+        pass
+
+# Subclass Airplane of Flight Vehicle, Grandchild of Vehicle
+class Airplane(FlightVehicle):
+    def init(self):
+        pass
+
+# Subclass Ground Vehicle of Vehicle
+class GroundVehicle(Vehicle):
+    def init(self):
+        pass
+    
+# Subclass Car of Ground Vehicle, Grandchild of Vehicle
+class Car(GroundVehicle):
+    def init(self):
+        pass 
+
+# Subclass Motorcycle of Ground Vehicle, Grandchild of Vehicle
+class Motorcycle(GroundVehicle):
+    def init(self):
+        pass 
